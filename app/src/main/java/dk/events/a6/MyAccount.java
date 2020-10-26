039295359a8d27b625236c1d28e00d33bef3b9f0
kasassulaiman <s195462@student.dtu.dk>
2020-10-26 23:53:54 +0100

    We have created profile page in MyAccount activity :)

--- app/src/main/java/dk/events/a6/MyAccount.java
+++ app/src/main/java/dk/events/a6/MyAccount.java
@@ -6,15 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MyAccount extends AppCompatActivity {
+public class MyAccount extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton back_arrow_account;
+    LinearLayout vis_profil,rediger,indstillinger;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+
+        vis_profil=findViewById(R.id.vis_profile_layout);
+        rediger=findViewById(R.id.rediger);
+        indstillinger=findViewById(R.id.indstillinger);
+
+        vis_profil.setOnClickListener(this);
+        rediger.setOnClickListener(this);
+        indstillinger.setOnClickListener(this);
 
         back_arrow_account = findViewById(R.id.back_arrow_account);
         back_arrow_account.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +40,21 @@ public class MyAccount extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()){
+            case R.id.vis_profile_layout:
+                Toast.makeText(getApplicationContext(),"View profile",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rediger:
+                Toast.makeText(getApplicationContext(),"Edit profile",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.indstillinger:
+                Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
 
