bea3ecc7a83c0dd003e8aa1aa4a534e06af820e1
kasassulaiman <s195462@student.dtu.dk>
2020-10-28 15:39:34 +0100

    ViewProfileActivity is ready

--- app/src/main/java/dk/events/a6/profileView/ViewProfileActivity.java
+++ app/src/main/java/dk/events/a6/profileView/ViewProfileActivity.java
@@ -7,6 +7,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +20,14 @@ public class ViewProfileActivity extends AppCompatActivity {
     private List<ProfileImage> profileImageList;
     private ProfilePhotoAdapter profilePhotoAdapter;
     private ViewPager2 viewPager2_profile_view;
+    private TextView number_of_image_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
         viewPager2_profile_view = findViewById(R.id.id_viewpager2_profile_view);
+        number_of_image_text=findViewById(R.id.number_of_image_text);
 
         profileImageList = new ArrayList<>();
         profileImageList.add(new ProfileImage(R.drawable.profile2));
@@ -36,6 +39,16 @@ public class ViewProfileActivity extends AppCompatActivity {
 
         profilePhotoAdapter = new ProfilePhotoAdapter(profileImageList,this);
         viewPager2_profile_view.setAdapter(profilePhotoAdapter);
+
+
+        viewPager2_profile_view.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                number_of_image_text.setText((position + 1 ) + " /" + profilePhotoAdapter.getItemCount());
+            }
+        });
+
 
     }
 
