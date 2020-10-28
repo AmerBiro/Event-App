4d425f899f67b55aeb85b138f0c10527db4176fd
kasassulaiman <s195462@student.dtu.dk>
2020-10-28 19:14:04 +0100

    change color of setting profile layout

--- app/src/main/java/dk/events/a6/profileView/EditProfileActivity.java
+++ app/src/main/java/dk/events/a6/profileView/EditProfileActivity.java
@@ -1,13 +1,10 @@
 package dk.events.a6.profileView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import dk.events.a6.R;
 
 public class EditProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
