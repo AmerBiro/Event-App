394841f7fdded9d29e8e968dcd6d5fef2af160ad
AmerBiro <amer.biro@gmail.com>
2020-11-22 12:35:43 +0000

    Adding sign out function to Profile settings

--- app/src/main/java/dk/events/a6/profileView/ProfileSettingsActivity.java
+++ app/src/main/java/dk/events/a6/profileView/ProfileSettingsActivity.java
@@ -2,15 +2,19 @@ package dk.events.a6.profileView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import dk.events.a6.R;
 import dk.events.a6.databinding.ActivityMainBinding;
 import dk.events.a6.databinding.ActivityProfileSettingsBinding;
 import dk.events.a6.databinding.ActivitySignUpBinding;
+import dk.events.a6.signInView.Registeration;
 
 public class ProfileSettingsActivity extends AppCompatActivity {
 
@@ -26,17 +30,6 @@ public class ProfileSettingsActivity extends AppCompatActivity {
         setContentView(view);
 
 
-
-        binding.buttonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Log out",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-
-
         binding.buttonDeleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,5 +38,15 @@ public class ProfileSettingsActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void signOut(View view) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        Intent intent = new Intent(ProfileSettingsActivity.this, Registeration.class);
+        Toast.makeText(this, "Accout logged out", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+        finish();
+        return;
     }
 }
