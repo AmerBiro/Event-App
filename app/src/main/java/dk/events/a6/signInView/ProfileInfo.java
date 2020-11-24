73f5a1d510121195d5f4c4cd57857f0b1f60245d
AmerBiro <amer.biro@gmail.com>
2020-11-24 17:55:16 +0000

    Adding ProfileInfo

---
+++ app/src/main/java/dk/events/a6/signInView/ProfileInfo.java
@@ -0,0 +1,27 @@
+package dk.events.a6.signInView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import dk.events.a6.R;
+import dk.events.a6.databinding.ActivityProfileInfoBinding;
+
+public class ProfileInfo extends AppCompatActivity {
+    private ActivityProfileInfoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_info);
+
+
+        binding = ActivityProfileInfoBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+    }
+}
