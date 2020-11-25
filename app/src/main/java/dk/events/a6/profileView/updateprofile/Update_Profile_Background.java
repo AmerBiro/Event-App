785eab426d75e6955c432d1dfe799d072d613ec5
AmerBiro <amer.biro@gmail.com>
2020-11-25 22:02:24 +0000

    Update ProfileInfo, Update_Profile_Background. A user can have no inserted data

--- app/src/main/java/dk/events/a6/profileView/updateprofile/Update_Profile_Background.java
+++ app/src/main/java/dk/events/a6/profileView/updateprofile/Update_Profile_Background.java
@@ -1,17 +1,12 @@
 package dk.events.a6.profileView.updateprofile;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,16 +18,13 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import dk.events.a6.R;
-import dk.events.a6.databinding.ActivityEditProfileBinding;
 import dk.events.a6.databinding.ActivityUpdateProfileBackgroundBinding;
 import dk.events.a6.profileView.MyAccount;
+import dk.events.a6.signInView.SelectProfileImages;
 
 public class Update_Profile_Background extends AppCompatActivity {
     private ActivityUpdateProfileBackgroundBinding binding;
@@ -87,22 +79,17 @@ public class Update_Profile_Background extends AppCompatActivity {
                 Education = value.getString("Education");
                 Description = value.getString("Description");
 
-                if (First_Name == null ||
-                        Last_Name == null ||
-                        Birthdate == null ||
-                        Gender == null ||
-                        Email == null ||
-                        Password == null ||
-                        Address == null ||
-                        Job == null ||
-                        Education == null ||
-                        Description == null){
-
-
-                    binding.Address.setText("");
-                    binding.Job.setText("");
-                    binding.Education.setText("");
-                    binding.Description.setText("");
+                if (
+                        Address == null &&
+                        Job == null &&
+                        Education == null &&
+                        Description == null
+                ){
+                    Address = ""; Job = ""; Education = ""; Description = "";
+                    binding.Address.setText(Address);
+                    binding.Job.setText(Job);
+                    binding.Education.setText(Education);
+                    binding.Description.setText(Description);
 
                 } else{
                     binding.Address.setText(Address);
@@ -111,7 +98,6 @@ public class Update_Profile_Background extends AppCompatActivity {
                     binding.Description.setText(Description);
                 }
             }
-
         });
 
 
@@ -126,13 +112,52 @@ public class Update_Profile_Background extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (
-                        binding.Address.getText().toString().isEmpty() ||
-                        binding.Job.getText().toString().isEmpty() ||
-                        binding.Education.getText().toString().isEmpty() ||
-                        binding.Description.getText().toString().isEmpty()
+                    binding.Address.getText().toString().isEmpty() &&
+                    binding.Job.getText().toString().isEmpty() &&
+                    binding.Education.getText().toString().isEmpty() &&
+                    binding.Description.getText().toString().isEmpty()
                 ){
-                    Toast.makeText(Update_Profile_Background.this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
+                    Address = ""; Job = ""; Education = ""; Description = "";
+
+                    user.updateEmail(Email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            DocumentReference dataRef = fStore.collection("Users").document(userID);
+                            Map<String, Object> updateUser = new HashMap<>();
+                            updateUser.put("First_Name", First_Name);
+                            updateUser.put("Last_Name", Last_Name);
+                            updateUser.put("Gender",Gender);
+                            updateUser.put("Birthdate", Birthdate);
+                            updateUser.put("Email", Email);
+                            updateUser.put("Password", Password);
+                            updateUser.put("Address", Address);
+                            updateUser.put("Job", Job);
+                            updateUser.put("Education", Education);
+                            updateUser.put("Description", Description);
+                            dataRef.set(updateUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+//                                    Toast.makeText(Update_Profile_Background.this, "Email is changed successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+//                                    Toast.makeText(Update_Profile_Background.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Update_Profile_Background.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    Toast.makeText(Update_Profile_Background.this, "All fields are empty. You can update your background info in /Update Profile Background/", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MyAccount.class));
+                    finish();
                     return;
+//                    return;
                 }
                 else {
                     user.updateEmail(Email).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -161,10 +186,6 @@ public class Update_Profile_Background extends AppCompatActivity {
 //                                    Toast.makeText(Update_Profile_Background.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             });
-                            Toast.makeText(Update_Profile_Background.this, "Profile background updated successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MyAccount.class));
-                            finish();
-                            return;
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
@@ -172,12 +193,12 @@ public class Update_Profile_Background extends AppCompatActivity {
                             Toast.makeText(Update_Profile_Background.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
+                    startActivity(new Intent(getApplicationContext(), MyAccount.class));
+                    finish();
+                    return;
                 }
             }
         });
-
-
-
 
     }
 }
