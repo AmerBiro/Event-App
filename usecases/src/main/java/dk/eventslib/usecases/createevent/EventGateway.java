1a1c5f40a09100abb870d792ccd33b7453736333
kpenov16 <s133967@student.dtu.dk>
2020-12-01 08:18:04 +0100

    get rid of the heroku event gateway impl as we moved to firebase

--- usecases/src/main/java/dk/eventslib/usecases/createevent/EventGateway.java
+++ usecases/src/main/java/dk/eventslib/usecases/createevent/EventGateway.java
@@ -5,18 +5,9 @@ import java.util.List;
 import dk.eventslib.entities.Event;
 
 public interface EventGateway {
-     //new
-     //License createLicense(License license);
-     //List<License> findLicensesForUserAndEvent(User user, Event event);
-
-     //User createUser(User user);
-     //User findUser(String userName);
-
      List<Event> findAllEvents();
-     void delete(Event event);
      Event findEventByTitle(String title);
-     //end new
-
+     void delete(Event event);
      Event createEvent(Event event);
 
      Event getEvent(String id);
